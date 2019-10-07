5a745983ee1a95f89056f91a8575507338fa3bde
Courtney Olesee <court.olson1@gmail.com>
2019-10-06 21:42:19 -0500

    finished required content

--- scripts/aboutMe.js
+++ scripts/aboutMe.js
@@ -22,31 +22,36 @@ let sentence = 'My name is ' + fullName + ' and I am ' + age + ' years old.';
 console.log(sentence);
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-let adventurous = true
+let adventurous = true;
 
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-let food = "burritos"
+let food = "burritos";
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-let pets = 2
+let pets = 2;
 
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-let friendsPets = 0
+let friendsPets = 0;
 
 // 11 - Add two pets to your `pets` variable
-pets += 2
+pets += 2;
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-let allowedPets = 3
+let allowedPets = 3;
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
-
-
+if (adventurous ) {
+  console.log("Adventures are great!");
+}
+else {
+  console.log("How about we stay home?");
+}
 
 // STRETCH GOALS:
 // 14 - Create a compound conditional: if age is greater than 18 and adventurous is true,
 // console log "Hurray!"
+
 
 // 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
